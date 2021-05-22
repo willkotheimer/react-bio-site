2daf69360d3dc63aab9b47af20df31220b00410d
wkotheimer <wkotheimer@gmail.com>
2021-05-21 22:44:52 -0500

    Some style fixes

--- src/Components/Bio/index.js
+++ src/Components/Bio/index.js
@@ -1,31 +1,22 @@
 import React, { useState } from 'react';
-import Wedding from '../../images/familyphotos/married.jpg';
-import Baby from '../../images/familyphotos/baby.jpg';
-import Sports from '../../images/familyphotos/sports.jpg';
-import ChristmasCard from '../../images/familyphotos/christmasCard.jpg';
-import ImageCard from '../Images';
+import Me from '../../images/me.jpg';
 
 export default function Bio(props) {
   const { bioData } = props;
-  const picArray = [[Wedding, 'Our Wedding', 'We were married in 2018'], [Sports, 'Max', 'Our baby was born in 2019'],
-    [Baby, 'Hobbies', 'Football Marvel and Code'], [ChristmasCard, 'Our First Christmas', 'Our first Christmas with our baby']];
   return (
-<section className="fullpage bio">
-    <div className="statement" id="bio">
-    <h1 class="neon">Family</h1>
-      <div className="family-section">
-        {picArray.map((pic) => <ImageCard imageUrl={pic} />)}
-      </div>
-      <div>
+<section id="bio" className="fullpage bio">
+    <div className="statement d-flex justify-content-between">
+    <h1 class="neon">About me</h1>
+      <div className="hero">
+        <img src={Me} />
+        <div>
         <h3>My Social Media</h3>
-        github - linkedin - instagram via Rachel
+        github - linkedin
+      </div>
       </div>
       <div className="personal">
-      <h1 class="neon">About me</h1>
       </div>
-      {bioData[0].info[0].personal}
       {bioData[0].info[1].professional}
-      <div id="projectAnchor"></div>
     </div>
 </section>
   );
--- src/Components/ProjectContainer/index.js
+++ src/Components/ProjectContainer/index.js
@@ -3,7 +3,7 @@ import ProjectCard from '../Card/index';
 
 export default function ProjectContainer({ projects }) {
   return (
-    <section id="projects" className="projects">
+    <section id="projectAnchor" className="projects">
     <h1 className="neon">My Projects:</h1>
     <div className="projectsContainer">
     {projects && projects.map((project, index) => (
