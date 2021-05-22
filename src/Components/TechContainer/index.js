00d412cb8ba2cda1356f1a4c7910bf044b47997d
wkotheimer <wkotheimer@gmail.com>
2021-05-22 00:00:03 -0500

    style changes

--- src/Components/TechContainer/index.js
+++ src/Components/TechContainer/index.js
@@ -4,7 +4,7 @@ import Tech from '../Tech';
 export default function TechContainer({ techData }) {
   return (
     <section id="techAnchor" className="tech">
-    <h1 className="neon">My Technologies:</h1>
+    <h1 className="font">My Technologies:</h1>
     <div className="techContainer d-flex flex-wrap justify-content-center">
     {Object.values(techData[0]).map((tech, index) => (
         <Tech key={index} tech={tech} />
