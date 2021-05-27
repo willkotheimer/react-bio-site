9b52caff8df247fcd3ec7535d9addfbfe7391d98
wkotheimer <wkotheimer@gmail.com>
2021-05-27 16:14:47 -0500

    Added front page modal and images

--- src/Components/Images/index.js
+++ src/Components/Images/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles({
   root: {
@@ -15,9 +19,34 @@ const useStyles = makeStyles({
   media: {
     height: 140,
   },
+  modalImage: {
+    width: '100%',
+    height: 'auto',
+  },
 });
 
 export default function ImageCard({ imageUrl }) {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
   const classes = useStyles();
   return (
     <Card className={`${classes.root} grow spacer`}>
@@ -33,6 +62,39 @@ export default function ImageCard({ imageUrl }) {
           <Typography variant="body2" color="textSecondary" component="p">
             {imageUrl[2]}
           </Typography>
+          <div>
+      <Button onClick={handleClickOpen('paper')}>Read More</Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">{imageUrl[1]}</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            <Typography style={ { color: '#10142d', whiteSpace: 'pre-line' }} >
+            { [...new Array(1)]
+              .map(
+                () => `${imageUrl[3]}`,
+              )
+              .join('\n') }
+            </Typography>
+              <img src={imageUrl[4]} className={`${classes.modalImage}`} />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            CLOSE
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
         </CardContent>
       </CardActionArea>
     </Card>
